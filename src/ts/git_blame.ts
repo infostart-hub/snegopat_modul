2039eac50c9cd86f3a5cf720d62bbb8012d55661
Orefkov Aleksander <orefkov@gmail.com>
2021-08-09 23:47:23 +0300

    Доработан скрипт git_blame.

--- src/ts/git_blame.ts
+++ src/ts/git_blame.ts
@@ -5,17 +5,32 @@
 //author: orefkov
 //help: inplace
 //addin: global
+//addin: vbs
 
 /// <reference path="./snegopat.d.ts" />
 /// <reference path="./v8.d.ts" />
 /*@
 @*/
 import * as stdlib from "./std/std";
-import * as stdcommands from "./std/commands";
+import * as git_common from "./git_common";
+declare var vbs: {var0: any, var1: any, var2: any, var3: any, var4: any, DoExecute(text: string): any};
 
-function getPathToRepo() {
-    return 'e:\\base_files\\';
-}
+type ParsedBlameLine = {
+    commit: string,
+    author: string,
+    timestamp: Date,
+    line: number,
+    text: string
+};
+
+type BlameForm = {
+    data: {mdObj: IV8MDObject, mdProp: IV8MDProperty, shift?: number, folder?: string, file?: string },
+    Controls: {Module: TextDocumentField, Title: Label, CmdBar: CommandBar} & Controls}
+    & Form;
+
+var openedForms: {[key: string]: BlameForm} = {};
+
+global.connectGlobals(SelfScript);
 
 function macrosПоказатьBlame() {
     var wnd = snegopat.activeTextWindow();
@@ -27,52 +42,120 @@ function macrosПоказатьBlame() {
         MessageBox("Это не окно модуля метаданных");
         return;
     }
-    debugger
-}
-
-/*
-function runAnalyses(td: TextDocument) {
-    try {
-        var tmpPath = GetTempFileName() + "\\";
-        CreateDirectory(tmpPath);
-        var fPath = tmpPath + "text.bsl";
-        td.Write(fPath);
-        if (!v8New("File", fPath).Существует()) {
-            MessageBox("Не удалось записать текст модуля во временный файл");
-            return undefined;
-        }
-        var wsh = new ActiveXObject("Wscript.Shell");
-        // todo - разобраться с путями с пробелами
-        var cmd = '"' + pathToServer + '" -a -s ' + tmpPath + ' -o ' + tmpPath +  ' -r json';
-        //Message(cmd);
-        wsh.Run(cmd, 1, 1);
-        fPath = tmpPath + "bsl-json.json";
-        if (!v8New("File", fPath).Существует()) {
-            MessageBox("Файл с результатом работы не найден");
-            DeleteFiles(tmpPath);
-            return undefined;
-        }
-        td = v8New("TextDocument");
-        td.Read(fPath, TextEncoding.UTF8);
-        DeleteFiles(tmpPath);
-        return JSON.parse(td.GetText());
-    } catch(e) {
-        Message(e.description);
-    }
-    return undefined;
-}
-*/
-var form;
-function loadForm() {
+    var sel = wnd.getSelection();
+    var form = processBlame(wnd.mdObj, wnd.mdProp, windows.getFocusedView().title);
     if (!form)
-        form = loadScriptFormEpf(SelfScript.fullPath.replace(/js$/i, "epf"), "Форма", SelfScript.self);
+        return;
+    form.Controls.Module.SetTextSelectionBounds(sel.beginRow, sel.beginCol + form.data.shift, sel.endRow, sel.endCol + form.data.shift);
 }
 
-function fillForm() {
-    loadForm();
-}
+function processBlame(mdObj: IV8MDObject, mdProp: IV8MDProperty, title: string) {
+    var pathes = git_common.pathes();
+    if (!pathes)
+        return null;
+    var fullPath = pathes.repo + pathes.src + git_common.mdObjToPath(mdObj, mdProp);
+    if (!stdlib.isFileExist(fullPath)) {
+        MessageBox(`Файл ${fullPath} не найден.`);
+        return null;
+    }
+    var split = /^(.+)\\([^\\]+)$/.exec(fullPath);
+    var res = git_common.runGit(split[1], "blame --date=unix " + split[2]);
+    if (res.err) {
+        Message("Произошла ошибка git: " + res.err);
+        return null;
+    }
 
-function openForm() {
-    loadForm();
+    var form = findOrLoadForm(mdObj, mdProp, title);
     form.Open();
+    var blameText = blameToText(parseGitBlame(res.out));
+    form.data.shift = blameText.shift;
+    form.Controls.Module.SetText(blameText.text);
+    form.Controls.Module.InsertLine(form.Controls.Module.LineCount() + 1, "");
+    return form;
+}
+
+function findOrLoadForm(mdObj: IV8MDObject, mdProp: IV8MDProperty, title: string) {
+    var key = mdObj.container.identifier + '\t' + mdObj.id + '\t' + mdProp.id;
+    var form = openedForms[key];
+    if (!form) {
+        form = git_common.loadGitForm("ПоказBlame", {
+            Refresh: () => {
+                processBlame(form.data.mdObj, form.data.mdProp, '');
+            },
+            GotoModule: function() {
+                var wnd = form.data.mdObj.openModule(form.data.mdProp.id);
+                var shift = form.data.shift;
+                vbs.var0 = form.Controls.Module;
+                vbs.DoExecute("var0.GetTextSelectionBounds var1, var2, var3, var4")
+                wnd.setCaretPos(vbs.var1, vbs.var2 - shift);
+                wnd.setSelection(vbs.var1, vbs.var2 - shift, vbs.var3, vbs.var4 - shift);
+            },
+            GotoModuleClose : function () {
+                this.GotoModule();
+                form.Close();
+            },
+            OnClose: function() {
+                delete openedForms[key];
+            }
+        }) as BlameForm;
+        form.Controls.CmdBar.Buttons.Goto.СочетаниеКлавиш = stdlib.v8hotkey(13, 0);
+        form.Controls.CmdBar.Buttons.GotoClose.СочетаниеКлавиш = stdlib.v8hotkey(13, 8);
+        form.Caption = "Git Blame: " + title;
+        form.data = {mdObj: mdObj, mdProp: mdProp};
+        form.Controls.Title.Caption = title;
+        openedForms[key] = form;
+    }
+    return form;
+}
+
+function padRight(text: string, len: number) {
+    var k = len - text.length;
+    if (k > 0)
+        text += new Array(k + 1).join(' ');
+    return text;
+}
+
+function blameToText(blame: ParsedBlameLine[]) {
+    var l: string[] = [];
+    var maxNameLen = 0, maxCommitLen = 0;
+    for (var i in blame) {
+        var b = blame[i];
+        if (b.author.length > maxNameLen)
+            maxNameLen = b.author.length;
+        if (b.commit.length > maxCommitLen)
+            maxCommitLen = b.commit.length;
+    }
+    // │ 2502
+    for (var i in blame) {
+        var b = blame[i];
+        l.push(`│ ${padRight(b.commit, maxCommitLen)} │ ${formatTime(b.timestamp)} │ ${padRight(b.author, maxNameLen)} │\t${b.text}`);
+    }
+    return {text: l.join('\n'), shift: 28 + maxCommitLen + maxNameLen};
+}
+
+function twoZeroes(n: number) {
+    return (n < 10 ? '0' : '') + n;
+}
+
+function formatTime(d: Date) {
+    return twoZeroes(d.getDay()) + '.' + twoZeroes(d.getMonth()) + '.' + d.getFullYear() + ' ' + twoZeroes(d.getHours()) + ':' + twoZeroes(d.getMinutes());
+}
+
+function parseGitBlame(text: string) {
+    var lines = text.split(/\r?\n/);
+    var reParse = /^\^?(\S+)\s+\((.+) (\d{10})\s+(\d+)\)(.*)/;
+    var res: ParsedBlameLine[] = [];
+    for (var i in lines) {
+        var lineParse = reParse.exec(lines[i]);
+        if (lineParse) {
+            res.push({
+                commit: lineParse[1],
+                author: lineParse[2],
+                timestamp: new Date(parseInt(lineParse[3]) * 1000),
+                line: parseInt(lineParse[4]),
+                text: lineParse[5]
+            });
+        }
+    }
+    return res;
 }
